8d657bd5ed2591e66c41028e6fd804985eb6a69f
svamsi56 <svkirshna56@gmail.com>
2019-09-14 17:30:18 -0400

    Added the navigation header for the application

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import React from "react";
 import "./App.css";
 import HomePage from "./Pages/homepage/homepage.component";
 import ShopPage from "./Pages/shop/shoppage.component";
+import Header from "./Components/header/header.component";
 import { Switch, Route } from "react-router-dom";
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
